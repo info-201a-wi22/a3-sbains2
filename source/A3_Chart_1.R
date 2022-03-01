23fbc0a9accdda26ede7d900ef3cd1f2143221a7
sbains2 <97136840+sbains2@users.noreply.github.com>
2022-03-01 13:30:15 -0800

    Add files via upload

--- source/A3_Chart_1.R
+++ source/A3_Chart_1.R
@@ -9,6 +9,13 @@ setwd("~/INFO-201code/a3-starter-sbains2/a3-sbains2/source")
 
 ## Graph #1: Time Trend of Total Incarcerations of Minorities and Whites in the United States from 1970 - 2016
 
+read_it <- read_csv("/Users/sahilbains/INFO-201code/a3-starter-sbains2/a3-sbains2/docs/incarceration_trends.csv")
+
+men_prison_max_race<- read_it %>%
+  group_by(year) %>%
+  summarize(white_male_prison_pop = sum(white_male_prison_pop, na.rm = TRUE),
+            black_male_prison_pop = sum(black_male_prison_pop, na.rm = TRUE)) %>% 
+  subset(black_male_prison_pop > 0)
 
 racial_incarceration_disparities_timespan <- ggplot(men_prison_max_race, aes(x = year)) +
   geom_line(mapping = aes(y = white_male_prison_pop, color = "white_male_prison_pop")) +
--- source/A3_Chart_2.R
+++ source/A3_Chart_2.R
@@ -1,5 +1,14 @@
 # Assignment 3 - Chart 2 - Comparison of two variables
 
+read_it <- read_csv("/Users/sahilbains/INFO-201code/a3-starter-sbains2/a3-sbains2/docs/incarceration_trends.csv")
+
+male_juvenile_adult_incarceration <- read_it %>%
+  group_by(year) %>%
+  summarize(male_adult_jail_pop = sum(male_prison_pop, na.rm = TRUE),
+            male_juvenile_jail_pop = sum(male_jail_pop, na.rm = TRUE)) %>%
+  subset(male_juvenile_jail_pop > 0,
+         male_adult_jail_pop > 0)
+
 male_juveline_adult_jail_chart <- ggplot(male_juvenile_adult_incarceration, aes(male_juvenile_adult_incarceration)) +
   geom_col(mapping = aes(x = year, 
                          y = male_adult_jail_pop, 
